8d6c91965e30369c82776a5d654f4a2e6980f4ef
Lessyz <102208615+Lessyzz@users.noreply.github.com>
2024-08-16 16:00:16 +0300

    App routes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_socketio import SocketIO, emit
-import string, random
+import string, random, sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -9,30 +9,65 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 username = ""
 rooms = {}
 
+#region Functions
+
+def createRoomVoid():
+    chars = string.ascii_letters + string.digits
+    roomid = ''.join(random.choice(chars) for _ in range(5))
+
+    while roomid in rooms:
+        roomid = ''.join(random.choice(chars) for _ in range(5))
+    return roomid
+
+#endregion
+
 #region Web Routes
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        global username
-        username = request.form['username']
-
-        karakterler = string.ascii_letters + string.digits
-        roomid = ''.join(random.choice(karakterler) for _ in range(5))
-    
-        return redirect(url_for('createroom', roomid = roomid))
     return render_template('index.html')
+
+#region Admin
+
+@app.route('/yonetici')
+def yonetici():
+    roomid = createRoomVoid()
+    return redirect(url_for('createroom', roomid = roomid))
 
 @app.route('/createroom/<roomid>')
 def createroom(roomid):
     return redirect(url_for('game', roomid = roomid))
 
+#endregion
+
+#region Player
+
+@app.route('/oyuncu', methods=['GET', 'POST'])
+def oyuncu():
+    if request.method == 'POST':
+        roomid = request.form['roomid']
+        return redirect(url_for('joinroom', roomid = roomid))
+    return render_template('oyuncu.html')
+
+@app.route('/joinroom/<roomid>')
+def joinroom(roomid):
+    return redirect(url_for('login', roomid = roomid))
+    
+@app.route('/login/<roomid>', methods=['GET', 'POST'])
+def login(roomid):
+    if request.method == 'POST':
+        global username
+        username = request.form['username']
+        return redirect(url_for('game', roomid = roomid))
+    return render_template('login.html', roomid=roomid)
+#endregion
+
 @app.route('/game/<roomid>')
 def game(roomid):
-    if roomid not in rooms: # Bu kişi admin oluyor
-        rooms[roomid] = username
+    if roomid not in rooms:                     # Bu kişi admin oluyor
+        rooms[roomid] = "token"
         return render_template('admin.html', username=username, roomid=roomid)
-    else: # Burada Oyuncu oluyor
+    else:                                       # Burada Oyuncu oluyor
         return render_template('game.html', username=username, roomid=roomid)
 
 #endregion
